3e59ee5762ab37da5d421d4983979797cf48d7ff
Accelerator <puteranikdimas@yahoo.com.sg>
2020-02-27 22:01:56 +0800

    Added Post Repository (WIP)
    Added Post Repository (WIP)

---
+++ Repositories/IPostRepository.cs
@@ -0,0 +1,21 @@
+﻿using DotNetCoreReactREST.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DotNetCoreReactREST.Repositories
+{
+    public interface IPostRepository
+    {
+        IEnumerable<Post> GetPosts { get; }
+        Post GetPostById(int postId);
+        IEnumerable<Post[]> GetPostByCategory(string category);
+        Post GetPostByIdAndCategory(int postId, string category);
+        void CreatePost(Post post);
+        void UpdatePost(Post post);
+        void DeletePost(int postId);
+        void Save();
+    }
+}
---
+++ Repositories/PostRepository.cs
@@ -0,0 +1,63 @@
+﻿using DotNetCoreReactREST.DbContexts;
+using DotNetCoreReactREST.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DotNetCoreReactREST.Repositories
+{
+    public class PostRepository : IPostRepository
+    {
+        private readonly AppDbContext _appDbContext;
+        public PostRepository(AppDbContext appDbContext)
+        {
+            this._appDbContext = appDbContext;
+        }
+
+        public IEnumerable<Post> GetPosts
+        {
+          get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public void CreatePost(Post post)
+        {
+            post.DateTime = DateTime.Now;
+            _appDbContext.Posts.Add(post);
+            Save();
+        }
+
+        public void DeletePost(int postId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Post[]> GetPostByCategory(string category)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Post GetPostById(int postId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Post GetPostByIdAndCategory(int postId, string category)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Save()
+        {
+            _appDbContext.SaveChangesAsync();
+        }
+
+        public void UpdatePost(Post post)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
