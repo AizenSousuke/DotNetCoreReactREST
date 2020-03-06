477ed82d689d553bbb296cc15fc6f01cac254c27
Avi Friedman <avifriedman01@gmail.com>
2020-03-05 22:46:52 -0500

    Reverted userRepo search functionality

--- Repositories/UserRepository.cs
+++ Repositories/UserRepository.cs
@@ -49,38 +49,7 @@ namespace DotNetCoreReactREST.Repositories
             return _context.Users.OrderBy(u => u.IsAdmin == true)
                 .OrderBy(u => u.UserName).ToList();
         }
-
-        public IEnumerable<ApplicationUser> GetUsers(UsersResourceParameters usersResourceParameters)
-        {
-            if (usersResourceParameters == null)
-            {
-                throw new ArgumentNullException(nameof(usersResourceParameters));
-            }
-
-            if (string.IsNullOrWhiteSpace(usersResourceParameters.SearchQuery))
-            {
-                return GetUsers();
-            }
-
-            var collection = _context.Authors as IQueryable<Author>;
-
-            if (!string.IsNullOrWhiteSpace(usersResourceParameters.MainCategory))
-            {
-                var mainCategory = usersResourceParameters.MainCategory.Trim();
-                collection = collection.Where(a => a.MainCategory == mainCategory);
-            }
-
-            if (!string.IsNullOrWhiteSpace(usersResourceParameters.SearchQuery))
-            {
-
-                var searchQuery = usersResourceParameters.SearchQuery.Trim();
-                collection = collection.Where(a => a.MainCategory.Contains(searchQuery)
-                    || a.FirstName.Contains(searchQuery)
-                    || a.LastName.Contains(searchQuery));
-            }
-
-            return collection.ToList();
-        }
+               
 
         public bool Save()
         {
