7d777f8d83c01e6d4b831c451ac3d926dd59bdc7
Accelerator <puteranikdimas@yahoo.com.sg>
2020-03-23 23:02:45 +0800

    Use Bogus package to create fake data instead of manually creating them.

--- DbContexts/AppDbContext.cs
+++ DbContexts/AppDbContext.cs
@@ -1,4 +1,5 @@
-﻿using DotNetCoreReactREST.Entities;
+﻿using Bogus;
+using DotNetCoreReactREST.Entities;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using System;
@@ -37,190 +38,83 @@ namespace DotNetCoreReactREST.DbContexts
                 .WithOne(c => c.Comment)
                 .OnDelete(DeleteBehavior.Cascade);
 
-            //modelBuilder.Entity<ApplicationUser>()
-            //    .HasMany(u => u.Posts)
-            //    .WithOne(p => p.ApplicationUser)
-            //    .HasForeignKey(p => p.ApplicationUserId)
-            //    .OnDelete(DeleteBehavior.Cascade);
+            // Bogus Fake Data =============
 
-            //modelBuilder.Entity<ApplicationUser>()
-            //    .HasMany(u => u.Comments)
-            //    .WithOne(c => c.ApplicationUser)
-            //    .HasForeignKey(c => c.ApplicationUserId)
-            //    .OnDelete(DeleteBehavior.Cascade);
+            // Set the randomizer seed if you wish to generate repeatable data sets.
+            Randomizer.Seed = new Random(123456);
 
-            //modelBuilder.Entity<Post>()
-            //   .HasMany(p => p.Comments)
-            //   .WithOne(c => c.Post)
-            //   .HasForeignKey(c => c.PostId)
-            //   .OnDelete(DeleteBehavior.Cascade);
+            var userId = 1;
 
-            //modelBuilder.Entity<Category>()
-            //   .HasMany(c => c.Posts)
-            //   .WithOne(p => p.Category)
-            //   .HasForeignKey(p => p.CategoryId)
-            //   .OnDelete(DeleteBehavior.Restrict);
+            // Users
+            var fakeUsers = new Faker<ApplicationUser>()
+                // Ensure all properties have rules. By default, StrictMode is false
+                // Set a global policy by using Faker.DefaultStrictMode
+                // .StrictMode(true)
+                // postId is deterministic
+                .RuleFor(o => o.Id, f => userId++.ToString())
+                .RuleFor(o => o.UserName, f => f.Name.FirstName())
+                .RuleFor(o => o.Email, f => f.Internet.Email(f.Name.FirstName()))
+                .RuleFor(o => o.PasswordHash, f => "password");
 
-            //modelBuilder.Entity<Comment>()
-            // .HasOne(c => c.ApplicationUser)
-            // .WithMany(u => u.Comments)
-            // .HasForeignKey(c => c.ApplicationUserId)
-            // .OnDelete(DeleteBehavior.NoAction);
+            var categoryId = 1;
 
-            //modelBuilder.Entity<Comment>()
-            // .HasOne(c => c.Post)
-            // .WithMany(p => p.Comments)
-            // .HasForeignKey(c => c.PostId)
-            // .OnDelete(DeleteBehavior.NoAction);
+            // Category
+            var fakeCategory = new Faker<Category>()
+                // Ensure all properties have rules. By default, StrictMode is false
+                // Set a global policy by using Faker.DefaultStrictMode
+                // .StrictMode(true)
+                // postId is deterministic
+                .RuleFor(o => o.Id, f => categoryId++)
+                .RuleFor(o => o.Name, f => f.Random.Word())
+                .RuleFor(o => o.Description, f => f.Random.Words());
 
-            // Seed data
+            var postId = 1;
+
+            // Posts
+            var fakePosts = new Faker<Post>()
+                // Ensure all properties have rules. By default, StrictMode is false
+                // Set a global policy by using Faker.DefaultStrictMode
+                // .StrictMode(true)
+                // postId is deterministic
+                .RuleFor(o => o.Id, f => postId++)
+                .RuleFor(o => o.CategoryId, f => f.Random.Int(1, 5))
+                .RuleFor(o => o.ApplicationUserId, f => f.Random.Int(1, 3).ToString())
+                .RuleFor(o => o.Title, f => f.Lorem.Sentence())
+                .RuleFor(o => o.Description, f => f.Lorem.Sentence())
+                .RuleFor(o => o.Content, f => f.Lorem.Sentence())
+                .RuleFor(o => o.DateTime, f => f.Date.Past());
+
+            var commentId = 1;
+
+            // Category
+            var fakeComment = new Faker<Comment>()
+                // Ensure all properties have rules. By default, StrictMode is false
+                // Set a global policy by using Faker.DefaultStrictMode
+                // .StrictMode(true)
+                // postId is deterministic
+                .RuleFor(o => o.Id, f => commentId++)
+                .RuleFor(o => o.ApplicationUserId, f => f.Random.Int(1, 3).ToString())
+                .RuleFor(o => o.PostId, f => f.Random.Int(1,3))
+                .RuleFor(o => o.Content, f => f.Random.Words())
+                .RuleFor(o => o.IsAnonymous, f => f.Random.Bool(0.5f));
+
+            // Generate the data
             modelBuilder.Entity<ApplicationUser>().HasData(
-                    new ApplicationUser { Id = "1", UserName = "JohnDoe", Email = "johnDoe@gmail.com", PasswordHash = "password" },
-                    new ApplicationUser { Id = "2", UserName = "Jane", Email = "Jane_Doe@gmail.com", PasswordHash = "password5" });
-            modelBuilder.Entity<Category>().HasData(new Category()
-            {
-                Id = 1,
-                Name = "Stet sit Category",
-                Description = "Sed justo exerci takimata nulla labore nonumy autem eirmod no"
-            });
+                fakeUsers.Generate(50)
+            );
 
-            modelBuilder.Entity<Category>().HasData(new Category()
-            {
-                Id = 2,
-                Name = "Aliquam consectetuer Category",
-                Description = "Sed justo exerci takimata nulla labore nonumy autem eirmod no"
-            });
+            modelBuilder.Entity<Category>().HasData(
+                fakeCategory.Generate(50)
+            );
 
-            modelBuilder.Entity<Category>().HasData(new Category()
-            {
-                Id = 3,
-                Name = "Et amet Category",
-                Description = "Sed justo exerci takimata nulla labore nonumy autem eirmod no"
-            });
+            modelBuilder.Entity<Post>().HasData(
+                fakePosts.Generate(50)
+            );
 
-            modelBuilder.Entity<Post>().HasData(new Post()
-            {
-                Id = 1,
-                CategoryId = 1,
-                ApplicationUserId = "1",
-                Title = "Autem nibh nulla nonumy lorem",
-                Content = "Consetetur ut lorem lorem imperdiet et nisl eos takimata te diam",
-                DateTime = DateTime.Now
-            });
-
-            modelBuilder.Entity<Post>().HasData(new Post()
-            {
-                Id = 2,
-                CategoryId = 1,
-                ApplicationUserId = "1",
-                Title = "Vero ipsum kasd in dolor",
-                Content = "Sea ullamcorper dolores tempor aliquyam sit sed diam elitr sed. Consetetur ut lorem lorem imperdiet et nisl eos takimata te diam",
-                DateTime = DateTime.Now.AddMinutes(3)
-            });
-
-            modelBuilder.Entity<Post>().HasData(new Post()
-            {
-                Id = 3,
-                CategoryId = 1,
-                ApplicationUserId = "2",
-                Title = "Eos dolores suscipsetetur dolores sadipscing eos lorem",
-                Content = "Nihil cum sit sanctus zzril. Consetetur ut lorem lorem imperdiet et nisl eos takimata te diam",
-                DateTime = DateTime.Now.AddMinutes(46)
-            });
-
-            modelBuilder.Entity<Post>().HasData(new Post()
-            {
-                Id = 4,
-                CategoryId = 1,
-                ApplicationUserId = "2",
-                Title = "Eos dolores suscipit consetetur dolores sadipscing eos lorem",
-                Content = "Nihil cum sit sanctus zzril. Consetetur ut lorem lorem imperdiet et nisl eos takimata te diam",
-                DateTime = DateTime.Now.AddMinutes(86)
-            });
-
-            modelBuilder.Entity<Post>().HasData(new Post()
-            {
-                Id = 5,
-                CategoryId = 1,
-                ApplicationUserId = "2",
-                Title = "Eos dolors sadipscing eos lorem",
-                Content = "Nihil cum sit sanctus zzril. Consetetur ut lorem lorem imperdiet et nisl eos takimata te diam",
-                DateTime = DateTime.Now.AddMinutes(56)
-            });
-
-            modelBuilder.Entity<Post>().HasData(new Post()
-            {
-                Id = 6,
-                CategoryId = 1,
-                ApplicationUserId = "2",
-                Title = "Eos dolores suscipit consetos lorem",
-                Content = "Nihil cum sit sanctus zzril. Consetetur ut lorem lorem imperdiet et nisl eos takimata te diam",
-                DateTime = DateTime.Now.AddMinutes(46)
-            });
-
-            modelBuilder.Entity<Post>().HasData(new Post()
-            {
-                Id = 7,
-                CategoryId = 1,
-                ApplicationUserId = "2",
-                Title = "Eotetur dolores sadipscing eos lorem",
-                Content = "Nihil cum sit sanctus zzril. Consetetur ut lorem lorem imperdiet et nisl eos takimata te diam",
-                DateTime = DateTime.Now.AddMinutes(36)
-            });
-
-            modelBuilder.Entity<Post>().HasData(new Post()
-            {
-                Id = 8,
-                CategoryId = 1,
-                ApplicationUserId = "2",
-                Title = "Eos dolores suscMisakaipscing eos lorem",
-                Content = "Nihil cum sit sanctus zzril. Consetetur ut lorem lorem imperdiet et nisl eos takimata te diam",
-                DateTime = DateTime.Now.AddMinutes(26)
-            });
-
-            modelBuilder.Entity<Post>().HasData(new Post()
-            {
-                Id = 9,
-                CategoryId = 1,
-                ApplicationUserId = "2",
-                Title = "Eos doloAJAJAJcing eos lorem",
-                Content = "Nihil cum sit sanctus zzril. Consetetur ut lorem lorem imperdiet et nisl eos takimata te diam",
-                DateTime = DateTime.Now.AddMinutes(16)
-            });
-            modelBuilder.Entity<Comment>().HasData(new Comment()
-            {
-                Id = 1,
-                ApplicationUserId = "1",
-                PostId = 1,
-                Content = "I think this post rocks but i'm known for bad judgement"
-
-            },
-            new Comment()
-            {
-                Id = 2,
-                ApplicationUserId = "2",
-                PostId = 1,
-                Content = "Your post sucks go home"
-
-            },
-            new Comment()
-            {
-                Id = 3,
-                ApplicationUserId = "2",
-                PostId = 3,
-                Content = "Cool Misaka!",
-                IsAnonymous = false
-
-            },
-            new Comment()
-            {
-                Id = 4,
-                ApplicationUserId = "1",
-                PostId = 4,
-                Content = "Cool Beans!",
-                IsAnonymous = true
-
-            });
+            modelBuilder.Entity<Comment>().HasData(
+                fakeComment.Generate(50)
+            );
+            // ==============================
         }
 
     }
